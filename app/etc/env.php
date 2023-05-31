715484fef114b2169501a4c1a0f62cf7ebde967b
Hai Phong <bjergsen169@gmail.com>
2023-05-31 21:22:47 +0700

    initial commit

---
+++ app/etc/env.php
@@ -0,0 +1,82 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_1pmlio'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => '6XJ2y4CkbOXzhf8Nq6P6dSeqPZ3z3K12'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'f73_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'f73_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '3dc4d7e575212f47ee5c05c3be2ed140'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => '127.0.0.1',
+                'dbname' => 'magento2',
+                'username' => 'magento2',
+                'password' => 'T@mtom169',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'install' => [
+        'date' => 'Mon, 29 May 2023 11:30:37 +0000'
+    ]
+];
---
+++ pub/static/frontend/Hiddentechies/next/en_US/Hiddentechies_Next/js/owl.carousel.js
@@ -0,0 +1,1 @@
+/var/www/html/magento2/app/code/Hiddentechies/Next/view/frontend/web/js/owl.carousel.js
